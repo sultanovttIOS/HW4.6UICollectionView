a3cb5631c76dbeabf21494d1e87d5219878843b8
sultanovttIOS <alisher192377@gmail.com>
2024-01-31 21:34:46 +0600

    Initial Commit

---
+++ HW4.6UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4.6UICollectionView
+//
+//  Created by Alisher Sultanov on 31/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
