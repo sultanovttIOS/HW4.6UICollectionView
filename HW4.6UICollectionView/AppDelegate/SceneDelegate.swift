79e62455f49eb9208e8a92ed495dcc119d25549b
sultanovttIOS <alisher192377@gmail.com>
2024-02-01 18:52:56 +0600

    Correct mistakes

--- HW4.6UICollectionView/AppDelegate/SceneDelegate.swift
+++ HW4.6UICollectionView/AppDelegate/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let nav1 = UINavigationController(rootViewController: SecondViewController())
+        let nav1 = UINavigationController(rootViewController: ViewController())
         window.rootViewController = nav1
         self.window = window
         self.window?.makeKeyAndVisible()
